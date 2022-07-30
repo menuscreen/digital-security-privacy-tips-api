f60d4a045d1ded5b20ce8b4ec918e2625059443d
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-07-30 16:45:11 -0400

    semi-colons

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express'); // ES6: import express from 'express';
 const app = express();
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 const PORT = 3000;
-require('dotenv').config()
+require('dotenv').config();
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
@@ -16,22 +16,22 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
 
     
 // extended allows you to create nested objects within your query strings, by surrounding the name of sub-keys with square brackets
-app.use(express.urlencoded({ extended: true })) 
+app.use(express.urlencoded({ extended: true }));
 
 // Response serving html landing page (used for api documentation)
 app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
-})
+    response.sendFile(__dirname + '/index.html');
+});
 // serving up json
 app.get('/api/:queryParameter', (req, res) => {
-    const queryParameter = req.params.queryParameter
+    const queryParameter = req.params.queryParameter;
     if (database[queryParameter]) {
-        res.json(database[queryParameter])
+        res.json(database[queryParameter]);
     } else {
-        res.json('404')
-    }
-})
+        res.json('404');
+    };
+});
 // process.env.PORT used for Heroku
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+    console.log(`Server listening on port ${PORT}`);
+});
